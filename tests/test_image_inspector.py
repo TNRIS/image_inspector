5282d947c36cc7d4bda5c61d94f1d8370c3de7d7
David Hickman <davidhickman13@gmail.com>
2017-05-04 09:41:40 -0500

    help test

--- tests/test_image_inspector.py
+++ tests/test_image_inspector.py
@@ -10,3 +10,28 @@ Tests for `image_inspector` module.
 
 import pytest
 
+from contextlib import contextmanager
+from click.testing import CliRunner
+
+from image_inspector import image_inspector
+from image_inspector import cli
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument.
+    """
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_command_line_interface():
+    runner = CliRunner()
+    help_result = runner.invoke(cli.inspect, ['--help'])
+    assert help_result.exit_code == 0
