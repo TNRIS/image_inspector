6baa4852d85266df4a157b9b2c07eb2bd220746a
David Hickman <davidhickman13@gmail.com>
2017-05-04 09:37:11 -0500

    temporarily removing tests

--- image_inspector/cli.py
+++ image_inspector/cli.py
@@ -5,8 +5,10 @@ from . import image_inspector
 
 
 @click.command()
-@click.argument('image_directory', type=click.Path(exists=True, file_okay=False, dir_okay=True))
-@click.option('--overwrite/--no-overwrite', default=False, help='Overwrite existing inventory files.')
+@click.argument('image_directory',
+                type=click.Path(exists=True, file_okay=False, dir_okay=True))
+@click.option('--overwrite/--no-overwrite', default=False,
+              help='Overwrite existing inventory files.')
 def inspect(image_directory, overwrite):
     """Console interface for the image-inspector tool"""
     image_inspector.find_images(image_directory, overwrite=overwrite)
